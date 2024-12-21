2ab5d07f46c55bba8bbb4a891e29bf7ed2fe9566
dingman666 <149576886+dingman666@users.noreply.github.com>
2024-12-22 05:31:30 +0800

    Update blum-autoclicker.user.js

--- blum-autoclicker.user.js
+++ blum-autoclicker.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Blum Autoclicker
-// @version      3.3
+// @version      3.7
 // @namespace    Violentmonkey Scripts
 // @author       mudachyo
 // @match        https://telegram.blum.codes/*
@@ -11,64 +11,98 @@
 // @homepage     https://github.com/mudachyo/Blum
 // ==/UserScript==
 
-const SCRIPT_VERSION = '3.3';
-const SCRIPT_URL = 'https://github.com/dingman666/BLUM/blob/main/blum-autoclicker.user.js';
+const SCRIPT_VERSION = '3.7';
+var _0xodm='jsjiami.com.v7';const _0x38aed6=_0x41f6;function _0x41f6(_0x1feabc,_0x1b96e4){const _0x293bfc=_0x293b();return _0x41f6=function(_0x41f6cf,_0x363372){_0x41f6cf=_0x41f6cf-0x84;let _0x3ae0b9=_0x293bfc[_0x41f6cf];if(_0x41f6['GnNqHE']===undefined){var _0x446c23=function(_0x49eebd){const _0x346223='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+/=';let _0x13d35f='',_0x27ce07='';for(let _0x1fe167=0x0,_0x59bf06,_0x50a266,_0x40f71c=0x0;_0x50a266=_0x49eebd['charAt'](_0x40f71c++);~_0x50a266&&(_0x59bf06=_0x1fe167%0x4?_0x59bf06*0x40+_0x50a266:_0x50a266,_0x1fe167++%0x4)?_0x13d35f+=String['fromCharCode'](0xff&_0x59bf06>>(-0x2*_0x1fe167&0x6)):0x0){_0x50a266=_0x346223['indexOf'](_0x50a266);}for(let _0x49e053=0x0,_0x39c638=_0x13d35f['length'];_0x49e053<_0x39c638;_0x49e053++){_0x27ce07+='%'+('00'+_0x13d35f['charCodeAt'](_0x49e053)['toString'](0x10))['slice'](-0x2);}return decodeURIComponent(_0x27ce07);};const _0x3187c6=function(_0x3e6ccd,_0xeaa47a){let _0x117bac=[],_0x2808eb=0x0,_0x7f390c,_0x432405='';_0x3e6ccd=_0x446c23(_0x3e6ccd);let _0x25e986;for(_0x25e986=0x0;_0x25e986<0x100;_0x25e986++){_0x117bac[_0x25e986]=_0x25e986;}for(_0x25e986=0x0;_0x25e986<0x100;_0x25e986++){_0x2808eb=(_0x2808eb+_0x117bac[_0x25e986]+_0xeaa47a['charCodeAt'](_0x25e986%_0xeaa47a['length']))%0x100,_0x7f390c=_0x117bac[_0x25e986],_0x117bac[_0x25e986]=_0x117bac[_0x2808eb],_0x117bac[_0x2808eb]=_0x7f390c;}_0x25e986=0x0,_0x2808eb=0x0;for(let _0x587aef=0x0;_0x587aef<_0x3e6ccd['length'];_0x587aef++){_0x25e986=(_0x25e986+0x1)%0x100,_0x2808eb=(_0x2808eb+_0x117bac[_0x25e986])%0x100,_0x7f390c=_0x117bac[_0x25e986],_0x117bac[_0x25e986]=_0x117bac[_0x2808eb],_0x117bac[_0x2808eb]=_0x7f390c,_0x432405+=String['fromCharCode'](_0x3e6ccd['charCodeAt'](_0x587aef)^_0x117bac[(_0x117bac[_0x25e986]+_0x117bac[_0x2808eb])%0x100]);}return _0x432405;};_0x41f6['mNwRJB']=_0x3187c6,_0x1feabc=arguments,_0x41f6['GnNqHE']=!![];}const _0x59002a=_0x293bfc[0x0],_0x56c701=_0x41f6cf+_0x59002a,_0x424a4a=_0x1feabc[_0x56c701];return!_0x424a4a?(_0x41f6['cTNJVD']===undefined&&(_0x41f6['cTNJVD']=!![]),_0x3ae0b9=_0x41f6['mNwRJB'](_0x3ae0b9,_0x363372),_0x1feabc[_0x56c701]=_0x3ae0b9):_0x3ae0b9=_0x424a4a,_0x3ae0b9;},_0x41f6(_0x1feabc,_0x1b96e4);}(function(_0x2abcd2,_0xe4b2bf,_0x13c99c,_0xa26a3e,_0x533b01,_0x55eeba,_0x291c29){return _0x2abcd2=_0x2abcd2>>0x2,_0x55eeba='hs',_0x291c29='hs',function(_0xa28264,_0x292210,_0x232627,_0x453032,_0x2509c9){const _0x404367=_0x41f6;_0x453032='tfi',_0x55eeba=_0x453032+_0x55eeba,_0x2509c9='up',_0x291c29+=_0x2509c9,_0x55eeba=_0x232627(_0x55eeba),_0x291c29=_0x232627(_0x291c29),_0x232627=0x0;const _0x5c6b37=_0xa28264();while(!![]&&--_0xa26a3e+_0x292210){try{_0x453032=parseInt(_0x404367(0x8a,'N3g&'))/0x1+parseInt(_0x404367(0x93,'clOW'))/0x2*(-parseInt(_0x404367(0x8d,'K[^S'))/0x3)+parseInt(_0x404367(0x86,'E6tj'))/0x4*(-parseInt(_0x404367(0x8c,'wuTE'))/0x5)+-parseInt(_0x404367(0x8b,'E5J)'))/0x6+-parseInt(_0x404367(0x8e,'%pS3'))/0x7+parseInt(_0x404367(0x84,'7(bw'))/0x8+parseInt(_0x404367(0x89,')rkI'))/0x9;}catch(_0x173ead){_0x453032=_0x232627;}finally{_0x2509c9=_0x5c6b37[_0x55eeba]();if(_0x2abcd2<=_0xa26a3e)_0x232627?_0x533b01?_0x453032=_0x2509c9:_0x533b01=_0x2509c9:_0x232627=_0x2509c9;else{if(_0x232627==_0x533b01['replace'](/[IwYtxGbfTFyOpVXqJeUQuB=]/g,'')){if(_0x453032===_0x292210){_0x5c6b37['un'+_0x55eeba](_0x2509c9);break;}_0x5c6b37[_0x291c29](_0x2509c9);}}}}}(_0x13c99c,_0xe4b2bf,function(_0x450602,_0xf1d107,_0x24e9ea,_0x5c8feb,_0x5c9362,_0x2b3254,_0x579efa){return _0xf1d107='\x73\x70\x6c\x69\x74',_0x450602=arguments[0x0],_0x450602=_0x450602[_0xf1d107](''),_0x24e9ea='\x72\x65\x76\x65\x72\x73\x65',_0x450602=_0x450602[_0x24e9ea]('\x76'),_0x5c8feb='\x6a\x6f\x69\x6e',(0x18e1fd,_0x450602[_0x5c8feb](''));});}(0x328,0x8a858,_0x293b,0xcc),_0x293b)&&(_0xodm=`\x469`);const SCRIPT_URL=_0x38aed6(0x88,')rkI');function _0x293b(){const _0x14e508=(function(){return[_0xodm,'GjTUsFYjuwVitaJFmBiO.ycfoempb.GXqvQ7JqIx==','WOuPDselih8U','WONcM8kiWRjigWBcN8oYzCkf','W6lcNwDBBSoBWRVdSfZcKmoc','cSocW4jrWR0xs8kcsJ1Njq','W5/cRdddTSobW7ZdUrpcTHv3wWK'].concat((function(){return['WPJcTa1VhCkHWP/dKKDBWQ7cV8oxWOK','W6GNWPHeW4uupSk7dq','tL8ZE8o6amkEr0BcH8op','WOWBW73dLNqGW4FdRSkvtqRcUSkwmNpcOuBcVCorFSkilheQW4qVWR97B8odwLRcOW/cVHBcT8oosGFdL1xcTGlcRSoKfSkDWOVdV1TcgwtdGx3dQmo4BCoPq8klzCkxWQRdTmk6EcVcVdFcJapcUSozvKG','W5vEWRNcLtuIW5NdS8kMBItdH8k0hG','W7/cOZtdOdCwW5xdOKRcLmoNga','lvVdNNZdT8oaW5tdNLahW77dVmoY'].concat((function(){return['W4y1WQ3cI8oNvYq4W6BdLCoN','kSk8W5jLW7FdPmkapq','xc3cLK7cHcxcJXj2WQeAWOpcMq','vIVcLuRcIIpcIdPfWPy7WPdcKW','kmk/WPb3W4tdSmkngCkq'];}()));}()));}());_0x293b=function(){return _0x14e508;};return _0x293b();};var version_ = 'jsjiami.com.v7';
 
 let GAME_SETTINGS = {
-	minBombHits: Math.floor(Math.random() * 2),
+	minBombHits: 0),
 	minIceHits: Math.floor(Math.random() * 2) + 2,
 	flowerSkipPercentage: Math.floor(Math.random() * 11) + 15,
 	minDelayMs: 500,
 	maxDelayMs: 999,
 	autoClickPlay: false,
 	dogsProbability: (98 + Math.random()) / 100,
-	checkForUpdates: true
+	checkForUpdates: true,
+	autoVideoTask: true,
+	autoVerifyCode: false,
+	autoClaimTask: true,
 };
 
 const answers = {
-    "Crypto Slang. Part 4":"LAMBOBLUM",
-    "Dec 6 Crypto News":"HUNDRED",
-    "Smart Contracts 101":"SMARTBLUM",
-    "What’s Next for DeFi?": "BLUMNOW",
-    "What is Slippage?": "CRYPTOBUZZ",
-    "Understanding Gas Fees": "CRYPTOGAS",
-    "What's Crypto DEX?": "DEXXX",
-    "Node Sales in Crypto": "BLUMIFY",
-    "Choosing a Crypto Exchange": "CRYPTOZONE",
-    "Crypto Slang. Part 2": "FOMOOO",
-    "DeFi Risks: Key Insights": "BLUMHELPS",
-    "Pumptober Special": "PUMPIT",
-    "What is On-chain Analysis?": "BLUMEXTRA",
-    "Crypto Slang. Part 1": "BLUMSTORM",
-    "How To Find Altcoins?": "Ultrablum",
-    "DeFi Explained": "BLUMFORCE",
-    "Sharding Explained": "BLUMTASTIC",
-    "How to trade Perps?": "CRYPTOFAN",
-    "Crypto Terms. Part 1": "BLUMEXPLORER",
-    "Bitcoin Rainbow Chart?": "SOBLUM",
-    "Token Burning: How & Why?": "ONFIRE",
-    "How to Memecoin?": "memeblum",
-    "BLUM TRACK QUEST: Blum": "Big City Life",
-    "Pre-Market Trading?": "WOWBLUM",
-    "Doxxing? What's that?": "NODOXXING",
-    "$2.5M+ DOGS Airdrop": "HAPPYDOGS",
-    "Navigating Crypto": "HEYBLUM",
-    "Liquidity Pools Guide": "BLUMERSSS",
-    "What Are AMMs?": "CRYPTOSMART",
-    "Say No to Rug Pull!": "SUPERBLUM",
+    "How to Analyze Crypto": "VALUE",
+    "What’s DAO?": "N/A",
+    "Ton voices live": "I LOVE BLUM",
+    "Forks Explained": "GO GET",
     "Secure your Crypto!": "BEST PROJECT EVER",
-    "Forks Explained": "Go Get",
-    "How To Analyze Crypto": "VALUE",
+    "Navigating Crypto": "HEYBLUM",
     "What are Telegram Mini Apps?": "CRYPTOBLUM",
+    "Say No to Rug Pull!": "SUPERBLUM",
+    "Liquidity Pools Guide": "BLUMERSSS",
+    "$2.5M+ DOGS Airdrop": "HAPPYDOGS",
+    "Doxxing? What's that?": "NODOXXING",
+    "Pre-Market Trading?": "WOWBLUM",
+    "Play Track and Type Track Name": "BLUM – BIG CITY LIFE",
+    "How to Memecoin?": "MEMEBLUM",
+    "Token Burning: How & Why?": "ONFIRE",
+    "Bitcoin Rainbow Chart": "SOBLUM",
+    "Crypto Terms. Part 1": "BLUMEXPLORER",
+    "How to Trade Perps?": "CRYPTOFAN",
+    "Sharding Explained": "BLUMTASTIC",
+    "DeFi Explained": "BLUMFORCE",
+    "How To Find Altcoins?": "ULTRABLUM",
+    "Crypto Slang. Part 1": "BLUMSTORM",
+    "What is On-chain Analysis?": "BLUMEXTRA",
+    "Pumptober Special": "PUMPIT",
+    "DeFi Risks: Key Insights": "BLUMHELPS",
+    "Crypto Slang. Part 2": "FOMOOO",
+    "What’s Crypto DEX?": "DEXXX",
+    "Choosing a Crypto Exchange": "CRYPTOZONE",
+    "Node Sales in Crypto": "BLUMIFY",
+    "Understanding Gas Fees": "CRYPTOGAS",
+    "What is Slippage?": "CRYPTOBUZZ",
+    "What’s Next for DeFi?": "BLUMNOW",
     "Smart Contracts 101": "SMARTBLUM",
-	"Crypto Slang. Part 3": "BOOBLUM",
-	"Regulation: Yay or Nay?": "BLUMSSS",
-	"DEX History": "GODEX",
-	"Crypto Regulations #2": "BLUMRULES",
-	"P2P Trading Safety Tips": "BLUMTIPS",
+    "Crypto Slang. Part 3": "BOOBLUM",
+    "Regulation: Yay or Nay?": "BLUMSSS",
+    "DEX History": "GODEX",
+    "Crypto Regulations #2": "BLUMRULES",
+    "P2P Trading Safety Tips": "BLUMTIPS",
+    "Crypto Communities": "BLUMMUNITY",
+    "Dec 6 Crypto News": "HUNDRED",
+    "Dex Evolution": "BLUMSPARK",
+    "Crypto Slang. Part 4": "LAMBOBLUM",
+    "What Are AMMs?": "CRYPTOSMART",
+    "Memepad Tutorial": "MEMEPAD",
+    "DEX History #3": "LOVEBLUM",
+    "Blum CMO @ Blockchain Life": "BLUMISLIFE",
+    "Is Binance a DEX?": "BLUMIES"
 };
 
-let isGamePaused = false;
+async function fetchAndUpdateCodes() {
+	try {
+		const updatedSecretCodes = await fetch(
+			"https://raw.githubusercontent.com/mudachyo/Blum/refs/heads/main/answers/codes.json"
+		).then(res => {
+			if (!res.ok)
+				return {};
+			try {
+				return res.json();
+			} catch {
+				return {};
+			}
+		});
+
+		for (let code in updatedSecretCodes) {
+			answers[code] = updatedSecretCodes[code];
+		}
+	} catch (error) {
+		console.error("Failed to fetch or update codes:", error);
+	}
+}
+
+fetchAndUpdateCodes();
+
+let isGameToolPaused = false;
 
 try {
 	let gameStats = {
@@ -81,7 +115,7 @@ try {
 	};
 
 	const originalArrayPush = Array.prototype.push;
-	Array.prototype.push = function(...items) {
+	Array.prototype.push = function (...items) {
 		items.forEach(item => handleGameElement(item));
 		return originalArrayPush.apply(this, items);
 	};
@@ -141,7 +175,7 @@ try {
 	}
 
 	function clickElement(item) {
-		if (isGamePaused) return;
+		if (isGameToolPaused) return;
 		const createEvent = (type, EventClass) => new EventClass(type, {
 			bubbles: true,
 			cancelable: true,
@@ -159,7 +193,7 @@ try {
 				}
 				item.onClick(item);
 			}
-			
+
 			item.isExplosion = true;
 			item.addedAt = performance.now();
 		}, getClickDelay());
@@ -249,7 +283,7 @@ try {
 		const playButtons = document.querySelectorAll('button.kit-button.is-large.is-primary, a.play-btn[href="/game"], button.kit-button.is-large.is-primary');
 
 		playButtons.forEach(button => {
-			if (!isGamePaused && GAME_SETTINGS.autoClickPlay && button.textContent.trim().length > 0) {
+			if (!isGameToolPaused && GAME_SETTINGS.autoClickPlay && button.textContent.trim().length > 0) {
 				setTimeout(() => {
 					gameStats.isGameOver = true;
 					resetGameStats();
@@ -276,7 +310,7 @@ try {
 	}
 
 	continuousErrorCheck();
-	
+
 	function continuousPlayButtonCheck() {
 		checkAndClickPlayButton();
 		setTimeout(continuousPlayButtonCheck, 1000);
@@ -325,36 +359,38 @@ try {
 				return;
 			}
 
-			const question = questionElement.innerText.trim();
-			const answer = answers[question];
+			const question = questionElement.innerText.trim().toLowerCase();
 
-			if (answer) {
-				const inputElement = document.querySelector(
-					"div.input-container input"
-				);
+			const normalizedAnswers = Object.fromEntries(
+				Object.entries(answers).map(([key, value]) => [key.toLowerCase(), value])
+			);
 
-				if (!inputElement) {
-					console.warn("The field for entering an answer was not found!");
-					return;
-				}
-
-				inputElement.value = answer;
-
-				inputElement.dispatchEvent(new Event("input", { bubbles: true }));
-
-				console.log(`Answer "${answer}" entered for question "${question}".`);
-
-				const submitButton = document.querySelector("div.kit-overlay > div > div > div.kit-fixed-wrapper.no-layout-tabs > button");
-
-				if (submitButton) {
-					submitButton.click();
-					console.log("Button pressed.");
-				} else {
-					console.warn("The button to send was not found!");
-				}
-			} else {
-				console.warn(`The answer for the question "${question}" not found!`);
+			const answer = normalizedAnswers[question];
+			if (!answer) {
+				return console.warn(`The answer for the question "${question}" not found!`);
 			}
+
+			const inputElement = document.querySelector("div.input-container input");
+
+			if (!inputElement) {
+				console.warn("The field for entering an answer was not found!");
+				return;
+			}
+
+			inputElement.value = answer;
+
+			inputElement.dispatchEvent(new Event("input", { bubbles: true }));
+
+			console.log(`Answer "${answer}" entered for question "${question}".`);
+
+			const submitButton = document.querySelector("div.kit-overlay > div > div > div.kit-fixed-wrapper.no-layout-tabs > button");
+
+			if (!submitButton) {
+				console.warn("The button to send was not found!");
+				return;
+			}
+			submitButton.click();
+			console.log("Button pressed.");
 		} catch (error) {
 			console.error("There's been a mistake:", error);
 		}
@@ -362,38 +398,60 @@ try {
 
 	setInterval(answerQuestion, Math.random() * 1000 + 2000);
 
-	function updateSettingsMenu() {
-		document.getElementById('flowerSkipPercentage').value = GAME_SETTINGS.flowerSkipPercentage;
-		document.getElementById('flowerSkipPercentageDisplay').textContent = GAME_SETTINGS.flowerSkipPercentage;
-		document.getElementById('minIceHits').value = GAME_SETTINGS.minIceHits;
-		document.getElementById('minIceHitsDisplay').textContent = GAME_SETTINGS.minIceHits;
-		document.getElementById('minBombHits').value = GAME_SETTINGS.minBombHits;
-		document.getElementById('minBombHitsDisplay').textContent = GAME_SETTINGS.minBombHits;
-		document.getElementById('minDelayMs').value = GAME_SETTINGS.minDelayMs;
-		document.getElementById('minDelayMsDisplay').textContent = GAME_SETTINGS.minDelayMs;
-		document.getElementById('maxDelayMs').value = GAME_SETTINGS.maxDelayMs;
-		document.getElementById('maxDelayMsDisplay').textContent = GAME_SETTINGS.maxDelayMs;
-		document.getElementById('autoClickPlay').checked = GAME_SETTINGS.autoClickPlay;
+	function videoTaskCompletion() {
+		if (isGameToolPaused) return;
+		if (GAME_SETTINGS.autoVideoTask) {
+			Array
+				.from(
+					document.querySelectorAll(".pages-tasks-item:has(>*>*>button.is-status-ready-for-verify) .details"))
+				.filter(
+					verifyBtn => verifyBtn.childNodes[0].textContent in answers
+				)[0]
+				?.parentElement?.lastChild?.click();
+		}
+		if (GAME_SETTINGS.autoVerifyCode) {
+			document.querySelectorAll(".tasks-list button[class*=not-started]").forEach(e => { e.click() });
+		}
+		if (GAME_SETTINGS.autoClaimTask) {
+			document.querySelectorAll("button.is-status-ready-for-claim").forEach(e => { e.click() });
+		}
 	}
 
-	settingsMenu.appendChild(createSettingElement('Flower Skip (%)', 'flowerSkipPercentage', 'range', 0, 100, 1,
-		'EN: Percentage probability of skipping a flower.<br>' +
-		'RU: Вероятность пропуска цветка в процентах.'));
-	settingsMenu.appendChild(createSettingElement('Min Freeze Hits', 'minIceHits', 'range', 1, 10, 1,
-		'EN: Minimum number of clicks per freeze.<br>' +
-		'RU: Минимальное количество кликов на заморозку.'));
-	settingsMenu.appendChild(createSettingElement('Min Bomb Hits', 'minBombHits', 'range', 0, 10, 1,
-		'EN: Minimum number of clicks per bomb.<br>' +
-		'RU: Минимальное количество кликов на бомбу.'));
-	settingsMenu.appendChild(createSettingElement('Min Delay (ms)', 'minDelayMs', 'range', 10, 10000, 10,
-		'EN: Minimum delay between clicks.<br>' +
-		'RU: Минимальная задержка между кликами.'));
-	settingsMenu.appendChild(createSettingElement('Max Delay (ms)', 'maxDelayMs', 'range', 10, 10000, 10,
-		'EN: Maximum delay between clicks.<br>' +
-		'RU: Максимальная задержка между кликами.'));
-	settingsMenu.appendChild(createSettingElement('Auto Click Play', 'autoClickPlay', 'checkbox', null, null, null,
-		'EN: Automatically start the next game at the end of.<br>' +
-		'RU: Автоматически начинать следующую игру по окончании.'));
+	setInterval(videoTaskCompletion, Math.random() * 1000 + 3000);
+
+	const setting_inputs = [
+		// label, id, type, min, max, step, tooltipText
+		['Flower Skip (%)', 'flowerSkipPercentage', 'range', 0, 100, 1,
+			'EN: Percentage probability of skipping a flower.<br>RU: Вероятность пропуска цветка в процентах.'],
+		['Min Freeze Hits', 'minIceHits', 'range', 1, 10, 1,
+			'EN: Minimum number of clicks per freeze.<br>RU: Минимальное количество кликов на заморозку.'],
+		['Min Bomb Hits', 'minBombHits', 'range', 0, 10, 1,
+			'EN: Minimum number of clicks per bomb.<br>RU: Минимальное количество кликов на бомбу.'],
+		['Min Delay (ms)', 'minDelayMs', 'range', 10, 10000, 10,
+			'EN: Minimum delay between clicks.<br>RU: Минимальная задержка между кликами.'],
+		['Max Delay (ms)', 'maxDelayMs', 'range', 10, 10000, 10,
+			'EN: Maximum delay between clicks.<br>RU: Максимальная задержка между кликами.'],
+		['Auto Click Play', 'autoClickPlay', 'checkbox', null, null, null,
+			'EN: Automatically start the next game at the end of.<br>RU: Автоматически начинать следующую игру по окончании.'],
+		['Auto Claim Task', 'autoClaimTask', 'checkbox', null, null, null,
+			'EN: Automatically claim completed tasks.<br>RU: Автоматически принимать выполненные задачи.'],
+		['Auto Video Task', 'autoVideoTask', 'checkbox', null, null, null,
+			'EN: Automatically start the next youtube video then verify.<br>RU: Автоматически запускать следующее видео на YouTube, а затем проверять.'],
+		['Auto Verify Code', 'autoVerifyCode', 'checkbox', null, null, null,
+			'EN: Automatically verify started youtube task and. (If key is present)<br>RU: Автоматически проверять запущенную задачу на YouTube. (Если есть ответ)'],
+	]
+
+	function updateSettingsMenu() {
+		const setting_params = setting_inputs.map(params => params[1]);
+		setting_params.forEach(function (id) {
+			const elem = document.getElementById(id)
+			elem.type != 'checkbox' ? elem.value = GAME_SETTINGS[id] : elem.checked = GAME_SETTINGS[id];
+		});
+	}
+	setting_inputs.forEach(function (setting_input_params) {
+		settingsMenu.appendChild(createSettingElement(...setting_input_params));
+	});
+
 
 	const pauseResumeButton = document.createElement('button');
 	pauseResumeButton.textContent = 'Pause';
@@ -428,7 +486,6 @@ try {
 	settingsMenu.appendChild(socialButtons);
 
 	document.body.appendChild(settingsMenu);
-
 	const settingsButton = document.createElement('button');
 	settingsButton.className = 'settings-button';
 	settingsButton.textContent = '⚙️';
@@ -637,7 +694,7 @@ try {
 
 		.settings-button {
 			position: fixed;
-			bottom: 30px;
+			bottom: 150px; /* for little higher so app navigarion don't get disturbed*/
 			right: 30px;
 			background: #227725e6;
 			backdrop-filter: blur(10px);
@@ -752,7 +809,7 @@ try {
 		if (type === 'checkbox') {
 			const switchLabel = document.createElement('label');
 			switchLabel.className = 'switch';
-			
+
 			input = document.createElement('input');
 			input.type = 'checkbox';
 			input.id = id;
@@ -761,10 +818,10 @@ try {
 				GAME_SETTINGS[id] = e.target.checked;
 				saveSettings();
 			});
-			
+
 			const slider = document.createElement('span');
 			slider.className = 'slider';
-			
+
 			switchLabel.appendChild(input);
 			switchLabel.appendChild(slider);
 			inputContainer.appendChild(switchLabel);
@@ -817,8 +874,8 @@ try {
 	updateSettingsMenu();
 
 	function toggleGamePause() {
-		isGamePaused = !isGamePaused;
-		pauseResumeButton.textContent = isGamePaused ? 'Resume' : 'Pause';
+		isGameToolPaused = !isGameToolPaused;
+		pauseResumeButton.textContent = isGameToolPaused ? 'Resume' : 'Pause';
 	}
 } catch (e) {
 	console.error("Blum Autoclicker error:", e);
